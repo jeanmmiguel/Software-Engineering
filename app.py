dfccf1f2c81a0dca13f49757d0b36d57e6f2c403
jeanmmiguel <jeanm@alunos.utfpr.edu.br>
2022-06-04 15:20:47 -0300

    commit das rotas

--- app.py
+++ app.py
@@ -4,36 +4,27 @@ from flask import Flask
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import datetime
 
 from flask import Response
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Our_data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cm_agenda.db'
 print ('Database opened')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-@app.route("/home")
-def hello_world():
-    allUsers = User.query.all()
-    Users = [i.serialize() for i in allUsers] 
-    return jsonify(
-        Users
-    )
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
-    divisao = db.Column(db.String(128))
-    stars = db.Column(db.Integer)
 
+    
     def serialize(self):
         return {
             'id': self.id,
             'name': self.name,
-            'divisao': self.divisao,
-            'stars': self.stars,
+         
         }
 
 
@@ -41,13 +32,60 @@ class User(db.Model):
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
-
+    duration_time = db.Column(db.DateTime(timezone=True))
+    price = db.Column(db.Float)
+    status = db.Column(db.Boolean)
     def serialize(self):
         return {
             'id': self.id,
             'name': self.name,
+            'duration_time': self.duration_time,
+            'price': self.price,
+            'status': self.status,
+            
+        }
+
+
+class Prefeitura(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+        
+    def serialize(self):
+        return {
+            'id': self.id,
+         
         }
 
 
 
+
+@app.route("/Home")
+def Mostrar_Eventos():
+    allEvents = Event.query.all()
+    Events = [i.serialize() for i in allEvents] 
+    return jsonify(
+        Events
+
+    )
+
+
+@app.route("/Admin")
+def Adm_Eventos():
+    Administrador = Prefeitura.query.all()
+    Administradores = [i.serialize() for i in Administrador] 
+    return jsonify(
+        Administradores
+        
+    )
+
+
+@app.route("/Usuarios")
+def hello_world():
+    allUsers = User.query.all()
+    Users = [i.serialize() for i in allUsers] 
+    return jsonify(
+        Users
+    )
+
 User.query.all()
+Event.query.all()
+Prefeitura.query.all()
